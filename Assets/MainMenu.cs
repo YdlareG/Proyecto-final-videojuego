05ee47ee3a79d1a43c1507e435e126c78c325a7b
Geraldy Nuñez Fernandez <geraldy2023@gmail.com>
2022-04-28 23:24:12 -0400

    geraldy

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+
+    public void EscenaJuego()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Tutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void MenuPrincipal()
+    {
+        SceneManager.LoadScene("mainMenu");
+    }
+}
---
+++ Assets/Target.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    [SerializeField] AudioClip acierto;
+    public void Hit()
+    {
+        AudioSource.PlayClipAtPoint(acierto, Camera.main.transform.position);
+        transform.position = TargetBounds.Instance.GetRandomPosition();
+    }
+}
